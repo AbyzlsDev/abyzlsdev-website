50279fcd927872cc2b0515f7265b116b7b2630d4
Abyzls <55506611+AbyzlsDev@users.noreply.github.com>
2022-08-20 12:09:07 +0300

    Updates

---
+++ precache-manifest.6c5498da8f68c3b9217090ee500c6af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b93a0e7c23a029a29c8c",
+    "url": "/abyzlsdev-website/static/css/main.a31af75e.chunk.css"
+  },
+  {
+    "revision": "b93a0e7c23a029a29c8c",
+    "url": "/abyzlsdev-website/static/js/main.b93a0e7c.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/abyzlsdev-website/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "4696bbb01e328b1a14d6",
+    "url": "/abyzlsdev-website/static/js/2.4696bbb0.chunk.js"
+  },
+  {
+    "revision": "74f0081b9d69425c5276",
+    "url": "/abyzlsdev-website/static/js/runtime~main.74f0081b.js"
+  },
+  {
+    "revision": "23598c231b0b153888d5b3c50688fb87",
+    "url": "/abyzlsdev-website/index.html"
+  }
+];
