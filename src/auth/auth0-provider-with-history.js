f8e1bfdbf5a280df2c32f7f33973924a9396f01a
AbyzlsDev <fake_skype@abv.bg>
2022-08-27 16:34:03 +0300

    login fixes

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -19,7 +19,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
       
       appState && appState.targetUrl
         ? appState.targetUrl
-        : window.location.href = 'http://localhost:3000'
+        : window.location.href = 'http://abyzlsdev.github.io/abyzlsdev-website'
     );
   };
 
